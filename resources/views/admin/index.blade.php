fd5a04ec76efd3eb042965fbc3ee3b67c51a39d8
edwinchp <chipineiro@gmail.com>
2020-04-22 23:55:31 -0500

    removed commented lines

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -11,12 +11,6 @@
 <link href="{{asset('css/libs.css')}}" rel="stylesheet">
 
 
-<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-<!--[if lt IE 9]>
-<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-<![endif]-->
 
 
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
