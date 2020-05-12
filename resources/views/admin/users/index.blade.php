6c38c9f79cff6f8a240b42ed58e34d291eec97db
edwinchp <chipineiro@gmail.com>
2020-05-11 22:52:45 -0500

    admin and index blades for admin moved. Displaying all Users

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -1,1 +1,39 @@
-Hi I am working!!!
+@extends('layouts.admin')
+
+
+@section('content')
+
+<h1>Users</h1>
+
+
+
+<table class="table">
+    <thead>
+        <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Name</th>
+            <th scope="col">Email</th>
+            <th scope="col">Created</th>
+            <th scope="col">Updated</th>
+        </tr>
+    </thead>
+    <tbody>
+
+        @if($users)
+
+        @foreach($users as $user)
+        <tr>
+            <th scope="row">{{$user->id}}</th>
+            <td>{{$user->name}}</td>
+            <td>{{$user->email}}</td>
+            <td>{{$user->created_at->diffForHumans()}}</td>
+            <td>{{$user->updated_at->diffForHumans()}}</td>
+        </tr>
+        @endforeach
+
+        @endif
+
+    </tbody>
+</table>
+
+@stop
