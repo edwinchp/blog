f753256ffea611a1068c95701dc378a3de64d3ec
edwinchp <chipineiro@gmail.com>
2020-05-12 23:50:14 -0500

    sidebar fixed and Mildred's login (js challenge)

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -36,4 +36,72 @@
     </tbody>
 </table>
 
+
+
+<div class="container">
+
+    <div class="formbox">
+        <span class="passlabel">Password</span>
+        <input type="password" placeholder="Insert your password here..." id="myPass">
+        <span id="showPass" onclick="toggle()">
+            <i class="fas fa-eye" onclick="myFunction(this)"></i>
+        </span>
+    </div>
+    
+</div>
+
+<style>
+    #showPass {
+        cursor: pointer;
+    }
+
+    @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap');
+
+    body, html{
+        margin: 0;
+        color: #34495e;
+        font-family: 'Roboto', sans-serif;
+        height: 100%;
+    }
+
+    input{
+        border-radius: 5px;
+        border: none;
+        background: #f6f6f6;
+        color: #666;
+        padding: 15px;
+        font-size: 1.3rem;
+    }
+
+    .passlabel{
+        display: block;
+        margin-bottom: 10px;
+        font-size: 1.5rem;
+    }
+
+    .container{
+        display: flex;
+        align-items: center;
+        justify-content: left;
+        /*height: 100vh;*/
+        background: #34495e;
+        background-size: cover;
+    }
+
+    .formbox{
+        background: white;
+        padding: 40px 50px 50px;
+        border-radius: 5px;
+        margin-top: 2rem;
+        margin-bottom: 2rem;
+    }
+
+    #showPass{
+        display: inline-block;
+        margin-left: -40px;
+    }
+
+
+</style>
+
 @stop
