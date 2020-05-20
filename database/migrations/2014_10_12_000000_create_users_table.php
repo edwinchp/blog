3133b45d2cafaf319a95dc9a90f102e81caeed6a
edwinchp <chipineiro@gmail.com>
2020-05-19 21:43:28 -0500

    photo_id added to migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,7 @@ class CreateUsersTable extends Migration
             $table->integer('is_active')->default(0);
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('photo_id')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -5,7 +5,7 @@
 
 <h1>Create User</h1>
 
-{!! Form::open(['method'=> 'POST', 'action'=>'AdminUsersController@store']) !!}
+{!! Form::open(['method'=> 'POST', 'action'=>'AdminUsersController@store', 'files' => true]) !!}
 
 <div class="form-group">
     {!! Form::label('name', 'Name')!!}
@@ -25,6 +25,11 @@
 <div class="form-group">
     {!! Form::label('is_active', 'Status')!!}
     {!! Form::select('is_active', array(1 => 'Active', 0 => 'Not Active'), 0, ['class'=>'form-control'])!!}
+</div>
+
+<div class="form-group">
+    {!! Form::label('file', 'File')!!}
+    {!! Form::file('file', null, ['class'=>'form-control'])!!}
 </div>
 
 <div class="form-group">
